e5ca19dfb0c79eebb731c504e189d987b1d7f50a
wiitcha <69749169+wiitcha@users.noreply.github.com>
2020-11-24 11:19:13 +0300

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");    // we get input
+    }
+    while (n < 1 || n > 8);    // the space that we didn't want
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int x = 0; n - (i + 1) > x; x++)        // spaces should repeat (n - i) times
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++)  //blocks
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
