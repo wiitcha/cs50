046d7b0ee25e68203b2366008f6ff604aad54ca3
wiitcha <69749169+wiitcha@users.noreply.github.com>
2020-11-24 11:25:21 +0300

    1.1

---
+++ Week 1/cash.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    float dollars;
+    do
+    {
+        dollars = get_float("Change owed: ");
+    }
+    while (dollars < 0);
+    int cents = round(dollars * 100);  // money conversion
+    int coins = 0;
+    coins += cents / 25;    
+    coins += (cents % 25) / 10;
+    coins += ((cents % 25) % 10) / 5;
+    coins += ((cents % 25) % 10) % 5;
+    printf("%i\n", coins);
+}
+
+
+
+
+
+    
+    
+           
+    
+
+
+
+
+
+
+
+
+
